e5f569164dfa39127a78a21e6229509ff077a746
kaykyu <kaiqing.97@outlook.com>
2023-10-31 12:27:20 +0800

    Separated respective code into repsective class

--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -1,35 +1,32 @@
 import java.io.Console;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintStream;
 import java.util.LinkedList;
-import java.util.Scanner;
 import java.nio.file.Path;
 
 public class ShoppingCart {
 
     static Path pNoUser = Path.of("db");
     static File fNoUser = new File (pNoUser.toString());
-    
+    static Path pUser = Path.of("cartdb");
+    static File fUser = new File (pUser.toString());
+    public static Console cons = System.console();
+    public static LinkedList<String> list = new LinkedList<>();
+    private static boolean userLogin = false;
+    private static String currentUser;
+
     public static File getfNoUser() {
         return fNoUser;
     }
-
-    static Path pUser = Path.of("cartdb");
-    static File fUser = new File (pUser.toString());
-
     public static File getfUser() {
         return fUser;
     }
 
-    public static Console cons = System.console();
-    public static LinkedList<String> list = new LinkedList<>();
-    public static boolean userLogin = false;
-    public static String user;
+    public static boolean isUserLogin() {
+        return userLogin;
+    }
 
     public static void list(LinkedList<String> cart) {
-         if (cart.size() == 0) {
+        if (cart.size() == 0) {
             System.out.println("Your cart is empty");
         } else {
             for (int i = 0; i < cart.size(); i++) {
@@ -44,10 +41,10 @@ public class ShoppingCart {
         for (String item : items) {
             String trimmedItem = item.trim();
             if (list.contains(trimmedItem)) {
-                System.out.println("You have " + trimmedItem + " in your cart");
+                System.out.println(String.format("You have %s in your cart", trimmedItem));
             } else {
                 list.add(trimmedItem);
-                System.out.println(trimmedItem + " added to cart");
+                System.out.println(String.format("%s added to cart", trimmedItem));
             }
         }
     }
@@ -60,76 +57,22 @@ public class ShoppingCart {
         } else {
             String item = list.get(index-1);
             list.remove(index-1);
-            System.out.println(item + " removed from cart");
-        }
-    }
-
-    public static void login(String input) {
-        userLogin = true;
-        list.clear();
-        String name = input.substring(6);
-        user = name;
-        createFile(name);
-    }
-
-    public static void createFile(String name) {
-        try {
-            File db = new File (getfUser(), String.format("%s.db.txt", name));
-            if (db.createNewFile()) {
-                System.out.println(String.format("%s, your cart is empty", name));
-            } else {
-                list = load(name);
-                System.out.println(String.format("%s, your cart contains the following items", name));
-                list(list);
-            }
-        } catch (IOException e) {
-            System.out.println("User cannot be created!");
-        }
-    }
-    
-    public static LinkedList<String> load(String name) {
-        File db = new File (getfUser(), String.format("%s.db.txt", name));
-        LinkedList<String> dbList = new LinkedList<>();
-        try {
-            Scanner reader = new Scanner(db);
-            while (reader.hasNextLine()) {
-                String data = reader.nextLine();
-                dbList.add(data);
-            }
-        reader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("User cart cannot be loaded!");
-        } return dbList;
-    }
-
-    public static void save() {
-        if (userLogin) {
-            try {
-                File db = new File (getfUser(), String.format("%s.db.txt", user));
-                PrintStream stdout = System.out;
-                PrintStream stream = new PrintStream(db);
-                System.out.println("Your cart has been saved");
-                System.setOut(stream);
-                for (int i = 0; i < list.size(); i++) {
-                    System.out.println(list.get(i));
-                }                
-                System.setOut(stdout);
-            } catch (FileNotFoundException e) {
-                System.out.println("User cart not found!");
-            }
-        } else {
-            System.out.println("Please login first");
+            System.out.println(String.format("%s removed from cart", item));
         }
     }
 
     public static void users() {
         String[] userList = fUser.list();
-        System.out.println("The following users are registered");         
-        for (String user : userList) {
-            String name = user.substring(0, user.indexOf("."));
-            System.out.println(name);
+        if (userList.length > 0) {
+            System.out.println("The following users are registered");         
+            for (String user : userList) {
+                String name = user.substring(0, user.indexOf("."));
+                System.out.println(name);
+            }
+        } else {
+            System.out.println("There are no registered users");
         }
-        }
+    }
     
 
     public static void main(String[] args) {
@@ -178,14 +121,23 @@ public class ShoppingCart {
                 delete(input.trim());
                 input = cons.readLine("> ");
             } else if (input.startsWith("login")) {
-                login(input.trim());
+                userLogin = true;
+                list.clear();
+                currentUser = input.substring(6);
+                ShoppingCartDB user = new ShoppingCartDB(currentUser);
+                user.createFile();
+                list = user.getCart();
                 input = cons.readLine("> ");
             } else if (input.startsWith("save")) {
-                save();
+                ShoppingCartDB user = new ShoppingCartDB(currentUser);
+                user.save();
                 input = cons.readLine("> ");
             } else if (input.startsWith("users")) {
                 users();
-                input= cons.readLine("> ");
+                input = cons.readLine("> ");
+            } else {
+                System.out.println("Invalid command");
+                input = cons.readLine("> ");
             }
         }
         System.out.println("Thank you for using Shopping Cart.");
--- src/ShoppingCartDB.java
+++ src/ShoppingCartDB.java
@@ -1,5 +1,80 @@
-public class ShoppingCartDB extends ShoppingCart {
-    
-}
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.LinkedList;
+import java.util.Scanner;
 
+public class ShoppingCartDB extends ShoppingCart {
+
+    private String user;
+    private LinkedList<String> cart = new LinkedList<>();    
+    private File userDB;
+    
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public LinkedList<String> getCart() {
+        return cart;
+    }
+
+    public ShoppingCartDB(String name) {
+        this.user = name;
+        this.cart = new LinkedList<String>();
+        this.userDB = new File (getfUser(), String.format("%s.db.txt", user));
+    }
+
+    public void createFile() {
+        try {
+            File db = userDB;
+            if (db.createNewFile()) {
+                System.out.println(String.format("%s, your cart is empty", user));
+            } else {
+                try {
+                    Scanner reader = new Scanner(db);
+                    while (reader.hasNextLine()) {
+                        String data = reader.nextLine();
+                        cart.add(data);
+                    }
+                    reader.close();
+                } catch (FileNotFoundException e) {
+                    System.out.println("User cart cannot be loaded!");
+                }
+                if (cart.size() > 0 ) {
+                    System.out.println(String.format("%s, your cart contains the following items", user));
+                    list(cart);
+                } else {
+                    System.out.println(String.format("%s, your cart is empty", user));
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("User cannot be created!");
+        }
+    }
+
+    public void save() {
+        if (isUserLogin()) {
+            try {
+                File db = userDB;
+                PrintStream stdout = System.out;
+                PrintStream stream = new PrintStream(db);
+                System.out.println("Your cart has been saved");
+                System.setOut(stream);
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println(list.get(i));
+                }                
+                System.setOut(stdout);
+            } catch (FileNotFoundException e) {
+                System.out.println("User cart not found!");
+            }
+        } else {
+            System.out.println("Please login first");
+        }
+    }
+}
 
